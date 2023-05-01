88b264738f1cdd4332fa7f293e7e6e37bbbe8f14
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-05-01 17:56:31 -0300

    Acessando a api dos livros e mostrando na tela

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var livros = [];
+var livrosConvertidos = [];
+var elementoInserirLivros = document.querySelector("#livros");
 
 var endPointLivros = "https://guilhermeonrails.github.io/casadocodigo/livros.json";
 
@@ -6,6 +7,25 @@ getBuscarLivrosAPI();
 
 async function getBuscarLivrosAPI(){
     var livros = await fetch(endPointLivros);
-    var livrosConvertidos = await livros.json();
+    livrosConvertidos = await livros.json();
     console.table(livrosConvertidos);
+    exibirOsLivrosNaTela(livrosConvertidos);
+}
+
+function exibirOsLivrosNaTela(listaDeLivros){
+    listaDeLivros.forEach(livro =>{
+        elementoInserirLivros.innerHTML += `
+        <div class="livro">
+      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.descricao}</p>
+      <p class="livro__preco" id="preco">${livro.preco}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>
+        `
+    })
 }
